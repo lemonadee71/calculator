acf18667144770f29abb41353e344cc99813a0f3
Shin Andrei Riego <riegoshinandrei@gmail.com>
2020-11-03 16:40:21 +0800

    First commit

---
+++ evaluate.js
@@ -0,0 +1,45 @@
+let test = '(5-2)^3!^-4!' || '-2+1+√4/2' || '-cos(10)*3+(5+2)^2' || '6+(-3*2)-(9-3)/3' || '((4*3)^2)/2' || '4*(3-2)+5' || '4^3^2*5'
+
+const factorial = (n) => {
+  if (n === 1) return 1
+  return n * factorial(n - 1)
+}
+
+const calculator = (type, x, y) => {
+  x = x ? evaluate(x) : null
+  y = y ? evaluate(y) : null
+
+  switch(type) {
+    case 'add':
+      return x + y;
+    case 'subtract':
+      return x - y;
+    case 'multiply':
+      return x * y;
+    case 'divide':
+      return x / y;
+    case 'exponent':
+      return x ** y; 
+    case 'factorial':
+      return factorial(x || y)
+    case 'negative':
+      return -1 * (x || y)
+    case 'sin':
+      return Math.sin(x || y)
+    case 'cos':
+      return Math.cos(x || y)
+    case 'tan':
+      return Math.tan(x || y)
+    case 'sqrt':
+      return Math.sqrt(x || y)
+  }
+}
+
+const evaluate = (node) => {
+  if (node.type === 'function' || node.type === 'operator')
+    return calculator(node.text, node.leftChild, node.rightChild)
+  return parseFloat(node.node)
+}
+
+//let tree = parse(test)
+//console.log(evaluate(tree))
---
+++ parser.js
@@ -0,0 +1,171 @@
+let operatorList = {
+  '(': {
+    type: 'bracket',
+    text: 'bracket',
+    precedence: 1
+  },
+  ')': {
+    type: 'bracket',
+    text: 'bracket',
+    precedence: 1
+  },
+  '+': {
+    type: 'operator',
+    text: 'add',
+    precedence: 2
+  },
+  '-': {
+    type: 'operator',
+    text: 'subtract',
+    precedence: 2
+  },
+  've': {
+    type: 'operator',
+    text: 'negative',
+    precedence: 3
+  },
+  '*': {
+    type: 'operator',
+    text: 'multiply',
+    precedence: 4
+  },
+  '/': {
+    type: 'operator',
+    text: 'divide',
+    precedence: 4
+  },
+  '^': {
+    type: 'function',
+    text: 'exponent',
+    precedence: 5
+  },
+  '!': {
+    type: 'function',
+    text: 'factorial',
+    precedence: 6
+  },
+  'sin': {
+    type: 'function',
+    text: 'sin',
+    precedence: 7
+  },
+  'cos': {
+    type: 'function',
+    text: 'cos',
+    precedence: 7
+  },
+  'tan': {
+    type: 'function',
+    text: 'tan',
+    precedence: 7
+  },
+  '√': {
+    type: 'function',
+    text: 'sqrt',
+    precedence: 5
+  },
+}
+
+function Node(x) {
+  this.node = x
+  this.parent = null
+  this.leftChild = null
+  this.rightChild = null
+  this.text = isNaN(this.node) ? operatorList[x].text : 'constant'
+  this.type = isNaN(this.node) ? operatorList[x].type : 'constant'
+  this.precedence = isNaN(this.node) ? operatorList[x].precedence : 7
+}
+
+const parse = (expr) => {
+  try {
+    let nodes = [],
+     prev = null
+
+    while(expr) {
+      let match = expr.match(/^[\d.]+/) || expr.match(/^[a-z]+/)
+      
+      if (match) {
+        if (isNaN(match[0])) {
+          if (match[0].includes('.'))
+            throw 'MathError. Decimal point is more than 1.'
+
+          nodes.push(new Node(match[0]))
+          expr = expr.replace(/^[a-z]+/, '')
+        } else {         
+          nodes.push(new Node(match[0]))
+          expr = expr.replace(/^[\d.]+/, '')
+        }
+
+        prev = match[0]
+      } else {
+        let operator = expr.charAt(0)
+
+        if (operator === '-' && !'()'.includes(prev) && isNaN(prev)) {
+          nodes.push(new Node('ve'))
+        } else {
+          nodes.push(new Node(operator))
+        }      
+
+        expr = expr.replace(operator, '')
+        prev = operator
+      }
+    }   
+
+    return createTree(nodes)
+  } catch(error) {
+    console.log(error)
+    return error
+  }
+  
+}
+
+const createTree = (nodes) => {
+  let expressionTree = nodes.reduce((tree, node) => insertToTree(tree, node), new Node('('))
+  
+  while(expressionTree.parent) {
+    expressionTree = expressionTree.parent
+  }
+
+  expressionTree = expressionTree.rightChild
+  expressionTree.parent = null
+  
+  return expressionTree
+}
+
+const insertToTree = (currentNode, newNode) => {
+  let condition = newNode.node === '^' || newNode.node === '√'
+                ? newNode.precedence < currentNode.precedence 
+                : newNode.precedence <= currentNode.precedence
+
+  if (newNode.node === ')') {
+    return deleteNode(currentNode)
+  }
+
+  if (newNode.text !== 'negative' && newNode.type !== 'bracket' && condition) {   
+    return insertToTree(currentNode.parent, newNode)
+  } else {
+    newNode.leftChild = currentNode.rightChild
+    currentNode.rightChild = newNode
+    newNode.parent = currentNode
+    
+    if (newNode.leftChild) {
+      newNode.leftChild.parent = newNode
+    }
+
+    return newNode
+  }
+}
+
+const deleteNode = (currentNode) => {
+  let openingParen = climbTree(currentNode)
+  openingParen.rightChild.parent = openingParen.parent
+  openingParen.parent.rightChild = openingParen.rightChild
+  return openingParen.parent
+}
+
+const climbTree = (currentNode) => {
+  if (currentNode.parent.type !== 'bracket') {
+    return climbTree(currentNode.parent)
+  }
+  return currentNode.parent
+}
